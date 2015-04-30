5233697014aadc2fcb0a1dd5c61be9047ab89833
shshnyl <haominhuthu@gmail.com>
2015-04-30 14:47:55 -0500

    tmp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main() {
     ifstream file_train(filename);
     if (!file_train.is_open()) 
         return -1;
+
     // create schema from the first line
     schema sc(NUMATTRS); 
     bool attrtype[] = {true, true, true, true, true, true, true, true, true, true, true, true, true};
@@ -27,32 +28,41 @@ int main() {
 
     // create instance line by line
     int count = 0; 
-    while (file_train.good() && count < 10) {
-        instance inst(NUMATTRS);
+    while (file_train.good()) {
+        instance *inst_ptr = new instance(NUMATTRS);
         getline(file_train, line); istringstream inst_parser(line);
-        for (int j = 0; j < NUMATTRS; j++) {
+        // attrs
+        for (int i = 0; i < NUMATTRS; i++) {
             getline(inst_parser, attr_val, ',');
             if (attr_val == "?") {
-                inst.attrs[j].attrAvail = false;
-                inst.attrs[j].attrVal = -1.0;
+                inst_ptr->attrs[i].attrAvail = true;//false;
+                inst_ptr->attrs[i].attrVal = -1.0;
             } 
             else {
-                inst.attrs[j].attrAvail = true;
+                inst_ptr->attrs[i].attrAvail = true;
                 istringstream tmp(attr_val);
-                tmp >> inst.attrs[j].attrVal;
-                cout << inst.attrs[j].attrVal + 100 << endl;
+                tmp >> inst_ptr->attrs[i].attrVal;
             }
         }
+        // flag
         getline(inst_parser, attr_val, ',');
-        cout << "flag: " << attr_val << endl;
-        if (attr_val == "1") 
-            inst.flag = true;
-        else 
-            inst.flag = false;
-        count++;
+        inst_ptr->flag = (attr_val == "1");
+        // insert into our dataset
+        if (attr_val != "") 
+            dataSet.push_back(*inst_ptr);
+        else {
+            delete inst_ptr;
+        }
     }
-
+    cout << dataSet.size() << endl << endl; 
     file_train.close();
+
+    // generate a decision tree
+    decisionTree dt(sc);
+    dt.train(dataSet);
+
+    //dt.printTree();
+
     return 0;
 }
 
