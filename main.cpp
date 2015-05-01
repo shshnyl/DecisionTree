cbd7badcb919adbd49426b01631d305531c52ee6
shshnyl <haominhuthu@gmail.com>
2015-04-30 21:14:37 -0500

    I just want to commit at this time

--- main.cpp
+++ main.cpp
@@ -9,15 +9,34 @@
     
 using namespace std;
 
+void parseFile(string filename, schema &sc, vector<instance> &dataset);
+
 int main() {
-    string filename = "btrain.csv", line, attr_val;
-    vector<instance> dataSet;
+    clock_t start, end;
+    vector<instance> dataset;
+    schema sc(NUMATTRS); 
+    parseFile("btrain.csv", sc, dataset);
+    // generate a decision tree
+    start = clock();
+    decisionTree dt(sc);
+    dt.train(dataset);
+    dt.printTree();
+    end = clock();
+    cout << "time to train the tree: " << double(end - start) / CLOCKS_PER_SEC << endl;
+    start = clock();
+    cout << "correct rate: " << dt.validate(dataset) << endl;
+    end = clock();
+    cout << "time to validate: " << double(end - start) / CLOCKS_PER_SEC << endl;
+    cout << "number of nodes: " << dt.countNodes() << endl; 
+    return 0;
+}
+
+void parseFile(string filename, schema &sc, vector<instance> &dataset) {
+    string line, attr_val;
     ifstream file_train(filename);
     if (!file_train.is_open()) 
-        return -1;
-
+        return ;
     // create schema from the first line
-    schema sc(NUMATTRS); 
     bool attr_type[] = {true, true, false, true, true, true, false, false, true, true, false, true, true};
     //bool attr_type[] = {true, true, true, true, true, true, true, true, true, true, true, true, true};
     getline(file_train, line); istringstream sc_parser(line);
@@ -27,7 +46,6 @@ int main() {
         sc.attrTypes[i] = attr_type[i];
     }
     getline(sc_parser, attr_val, ','); // get rid of the "winner"
-
     // create instance line by line
     int count = 0; 
     while (file_train.good()) {
@@ -50,28 +68,15 @@ int main() {
         getline(inst_parser, attr_val, ',');
         inst_ptr->flag = (attr_val == "1");
         // insert into our dataset
-        if (attr_val != "") 
-            dataSet.push_back(*inst_ptr);
+        if (attr_val != "" && attr_val != "?") 
+            dataset.push_back(*inst_ptr);
         else {
             delete inst_ptr;
         }
     }
-    cout << dataSet.size() << endl << endl; 
+    cout << dataset.size() << endl << endl; 
     file_train.close();
-
-    // generate a decision tree
-    clock_t start, end;
-    start = clock();
-    decisionTree dt(sc);
-    dt.train(dataSet);
-    dt.printTree();
-    end = clock();
-    cout << "time to train the tree: " << double(end - start) / CLOCKS_PER_SEC << endl;
-    start = clock();
-    cout << "correct rate: " << dt.validate(dataSet) << endl;
-    end = clock();
-    cout << "time to validate: " << double(end - start) / CLOCKS_PER_SEC << endl;
-    cout << "number of nodes: " << dt.countNodes() << endl; 
-    return 0;
 }
+
+
 
